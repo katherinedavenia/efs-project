f264eb16fb5ec5e2731f2a0bc3412e0b97d6f044
Katherine Davenia <katherinedavenia24@gmail.com>
2022-11-16 16:24:45 +0700

    fix/fix cli setup using yargs

--- packages/server/src/cli.js
+++ packages/server/src/cli.js
@@ -1,7 +1,17 @@
-function cli(args) {
-  console.log(args);
-}
+const yargs = require("yargs");
 
-module.exports = {
-  cli,
-};
+const options = yargs
+  .usage("fishery")
+  .option("l", {
+    alias: "language",
+    describe: "Translate to language",
+    type: "string",
+    demandOption: false,
+  })
+  .option("s", {
+    alias: "sentence",
+    describe: "Sentence to be translated",
+    type: "string",
+    demandOption: false,
+  })
+  .help(true).argv;
